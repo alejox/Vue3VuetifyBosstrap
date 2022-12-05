e86ef1bda14493c0159dfb87d6990a22878d32dd
camilo garcia <camilo456garcia@gmail.com>
2022-12-05 12:40:54 -0500

    modify base template

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { loadFonts } from "./plugins/webfontloader";
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import './assets/main.scss'
 
+import VueTelInput from 'vue-tel-input';
+import 'vue-tel-input/dist/vue-tel-input.css';
 loadFonts();
 
-createApp(App).use(router).use(store).mount("#app");
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(BootstrapVue3)
+app.use(VueTelInput)
+app.use(VueApexCharts)
+app.mount("#app");
